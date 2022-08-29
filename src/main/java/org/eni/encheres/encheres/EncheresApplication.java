bad163cc400de8b026ca710d622fb09d2d615782
Roland Sainty <roland.sainty2022@campus-eni.fr>
2022-08-29 15:11:17 +0200

    oups

---
+++ src/main/java/org/eni/encheres/encheres/EncheresApplication.java
@@ -0,0 +1,13 @@
+package org.eni.encheres.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eni/encheres/encheres/EncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eni.encheres.encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
